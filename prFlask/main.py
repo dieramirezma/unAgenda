f3de169b11d81f96a755e59ff700357898c8864e
Felipe <ainfanteh@unal.edu.co>
2023-09-09 15:29:33 -0500

    Login / register, version 1.0

---
+++ prFlask/main.py
@@ -0,0 +1,97 @@
+# Importación de los módulos necesarios
+from flask import Flask, render_template, redirect, request, session
+from flask_mysqldb import MySQL, MySQLdb
+
+# Crear una instancia de la aplicación Flask
+app = Flask(__name__, template_folder='templates')
+
+# Configuración de la conexión a la base de datos MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'prFlask'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Inicialización de la extensión MySQL
+mysql = MySQL(app)
+
+# Definición de la ruta para la página de inicio
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+# Definición de la ruta para la página de administrador
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+# Ruta para el proceso de inicio de sesión
+@app.route('/loginAccess', methods=["GET", "POST"])
+def login():
+    # Verificar si se ha enviado un formulario POST
+    if request.method == 'POST' and 'txtEmail' in request.form and 'txtPassword' in request.form:
+        # Obtener el correo y la contraseña proporcionados por el usuario
+        _correo = request.form['txtEmail']
+        _contraseña = request.form['txtPassword']
+
+        # Crear un cursor para la base de datos MySQL
+        cur = mysql.connection.cursor()
+
+        # Ejecutar una consulta SQL para buscar un usuario con el correo y contraseña proporcionados
+        cur.execute('SELECT * FROM usuario WHERE correo = %s AND contraseña = %s', (_correo, _contraseña))
+        
+        # Obtener la primera fila (si existe) que coincide con la consulta
+        account = cur.fetchone()
+        
+        # Si se encuentra un usuario con éxito
+        if account:
+            # Establecer una sesión de usuario marcándolo como logueado y almacenando su ID de usuario
+            session['logueado'] = True
+            session['idUsuario'] = account['idUsuario']
+            
+            # Redirigir al usuario a la página de administrador
+            return render_template('admin.html')
+        else:
+            # Si no se encuentra un usuario, redirigir de nuevo a la página de inicio
+            return render_template('index.html')
+
+# Ruta para el proceso de registro
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'POST' and 'txtEmail' in request.form and 'txtPassword' in request.form:
+        # Obtener el correo y la contraseña proporcionados por el usuario
+        _correo = request.form['txtEmail']
+        _contraseña = request.form['txtPassword']
+        _nombre = request.form['txtNombre']
+
+        # Crear un cursor para la base de datos MySQL
+        cur = mysql.connection.cursor()
+
+        # Verificar si el correo ya está en uso
+        cur.execute('SELECT * FROM usuario WHERE correo = %s', [_correo])
+        existing_user = cur.fetchone()
+
+        # Si el correo ya está en uso, mostrar un mensaje de error
+        if existing_user:
+            return render_template('register.html', message='El correo ya está en uso.')
+
+        # Si el correo no está en uso, insertar el nuevo usuario en la base de datos
+        cur.execute('INSERT INTO usuario (nombre, correo, contraseña) VALUES (%s, %s, %s)', (_nombre, _correo, _contraseña))
+        mysql.connection.commit()
+
+        # Establecer una sesión de usuario para el nuevo usuario registrado
+        session['logueado'] = True
+        session['idUsuario'] = cur.lastrowid
+
+        # Redirigir al usuario a la página de administrador
+        return render_template('admin.html')
+
+    # Si se accede al registro por GET o no se proporcionan datos válidos, mostrar el formulario de registro
+    return render_template('register.html')
+
+# Configuración de la clave secreta para las sesiones de usuario
+if __name__ == '__main__':
+    app.secret_key = "prFlask"
+
+# Ejecución de la aplicación Flask
+app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
