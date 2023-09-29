fdf385d5da5f048435b45740d3126066bebc6032
John Andres Rua <110644754+johnrua17@users.noreply.github.com>
2023-09-29 17:17:02 -0500

    Update main.py

--- prFlask/main.py
+++ prFlask/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder='templates')
 # Configuración de la conexión a la base de datos MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '22446688Rengifo'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'prFlask'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
