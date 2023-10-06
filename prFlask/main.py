6a0bc362211a8851ac3417a020d747777f4e79a9
Diego Nicolás Ramírez <81760886+dieramirezma@users.noreply.github.com>
2023-10-06 16:22:20 -0500

    Update main.py

--- prFlask/main.py
+++ prFlask/main.py
@@ -9,8 +9,7 @@ app = Flask(__name__, template_folder='templates')
 # Configuración de la conexión a la base de datos MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'db12dieramirezma'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'prFlask'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
