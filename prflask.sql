261c378b33b9906146ab98c432a93ea34de6702b
TiliAyala <iayalar@unal.edu.co>
2023-10-11 18:25:07 -0500

    Funcionalidad de Borrado de notas Implementado

--- prflask.sql
+++ prflask.sql
@@ -19,7 +19,7 @@ USE prflask;
 --
 -- Table structure for table `usuario`
 --
-
+/*
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
@@ -84,3 +84,80 @@ CREATE TABLE horario (
 );
 
 SELECT * FROM horario;
+*/
+
+DROP TABLE IF EXISTS `horario`;
+DROP TABLE IF EXISTS `respuestas_seguridad`;
+DROP TABLE IF EXISTS `grupoNotas`;
+DROP TABLE IF EXISTS `usuario`;
+
+
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contraseña` varchar(50) NOT NULL,
+  `codigo_verificacion` VARCHAR(255),
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `correo` (`correo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Usuario1','usuario1@example.com','pass1', 'abc'),(2,'Usuario2','usuario2@example.com','pass2', 'abc'),(3,'Usuario3','usuario3@example.com','pass3', 'abc');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- Dump completed on 2023-09-09 15:24:54
+
+-- para crear notas en el bloc (en proceso)
+DROP TABLE IF EXISTS `grupoNotas`;
+CREATE TABLE grupoNotas (
+    idNota INT AUTO_INCREMENT PRIMARY KEY,
+    idUsuario INT,
+    numGrupo INT,
+    porcentaje FLOAT,
+    nota FLOAT,
+    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
+);
+
+SELECT * FROM grupoNotas;
+DELETE FROM grupoNotas WHERE idNota < 100;
+
+-- CREACIÓN DE LOS EVENTOS DEL HORARIO --
+
+CREATE TABLE horario (
+    `idHorario` INT(11) NOT NULL AUTO_INCREMENT,
+    `idUsuario` INT(11) NOT NULL,
+    `evento` VARCHAR(255) NOT NULL,
+    `horaInicio` INT(5) NOT NULL,
+    `horaFin` INT(5) NOT NULL,
+    `diaSemana` VARCHAR(15) NOT NULL,
+    PRIMARY KEY (`idHorario`),
+    FOREIGN KEY (`idUsuario`) REFERENCES usuario(`idUsuario`)
+);
+
+SELECT * FROM horario;
+
+-- RESPUESTAS DE SEGURIDAD -- 
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `respuestas_seguridad` (
+  `idRespuesta` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) DEFAULT NULL,
+  `respuesta1` varchar(255) NOT NULL,
+  `respuesta2` varchar(255) NOT NULL,
+  `respuesta3` varchar(255) NOT NULL,
+  PRIMARY KEY (`idRespuesta`),
+  KEY `idUsuario` (`idUsuario`),
+  CONSTRAINT `respuestas_seguridad_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
