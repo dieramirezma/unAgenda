0abb1f6d20da6ff36a0be80a4e6f2b3d80f22161
AndersonMorales08 <amoralesch@unal.edu.co>
2023-10-12 17:40:11 -0500

    Bases de datos actualizada.

--- prflask.sql
+++ prflask.sql
@@ -84,6 +84,7 @@ CREATE TABLE `respuestas_seguridad` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+SELECT * FROM respuestas_seguridad;   
 --
 -- Dumping data for table `respuestas_seguridad`
 --
