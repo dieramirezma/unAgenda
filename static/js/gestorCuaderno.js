d81601c637e54fd10d9034c63a96d497401f1a4d
AndersonMorales08 <amoralesch@unal.edu.co>
2023-11-03 16:09:31 -0500

    Se mejoró el filtro para actualizar el cuaderno en la base de datos

--- static/js/gestorCuaderno.js
+++ static/js/gestorCuaderno.js
@@ -1,5 +1,8 @@
 const btn_save = document.querySelector(".btn-guardar");
 const btn_change = document.querySelector(".btn-cambiar");
+const nombre_cuaderno = document.querySelector("#encabezado-nombre");
+const id_cuaderno = document.querySelector("#id-cuaderno");
+
 
 let globalContent;
 
@@ -7,17 +10,25 @@ let identificador = setInterval(save_notebook, 60000)
 
 async function save_notebook() {
     let myContent = tinymce.get("mytextarea").getContent();
+    let nombre = nombre_cuaderno.textContent;
+    let id = id_cuaderno.textContent;
+
+    id = id.trim()
+
     globalContent = myContent;
     myContent = myContent.replaceAll('"', "'");
     myContent = myContent.replaceAll('&', '5hjis6754');
     myContent = myContent.replaceAll(',', '5hjdf4754');
 
     content = myContent.split('\n');
-    console.log(content)
+    console.log(nombre, id)
+
+    let url = '/cuaderno?contenido=' + content + '&nombre=' + nombre + '&id=' + id;
+    console.log(nombre.length);
 
     try {
 
-        const reponse = await fetch('/cuaderno?contenido=' + content);
+        const reponse = await fetch(url);
         console.log(reponse);
 
     } catch (error) {
